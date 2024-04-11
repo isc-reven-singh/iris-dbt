d66a73551f925864a107657cfe7536b6f2832f47
isc-reven-singh <reven.singh@intersystems.com>
2024-04-11 09:35:04 +0200

    Initial commit Changes committed: 	new file:   sales/.gitignore 	new file:   sales/README.md 	new file:   sales/analyses/.gitkeep 	new file:   sales/dbt_project.yml 	new file:   sales/macros/.gitkeep 	new file:   sales/models/example/my_first_dbt_model.sql 	new file:   sales/models/example/my_second_dbt_model.sql 	new file:   sales/models/example/schema.yml 	new file:   sales/profiles.yml 	new file:   sales/seeds/.gitkeep 	new file:   sales/snapshots/.gitkeep 	new file:   sales/tests/.gitkeep 	new file:   salesdag.py

---
+++ salesdag.py
@@ -0,0 +1,26 @@
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.config import ProfileConfig
+import os
+from datetime import datetime
+
+profile_config = ProfileConfig(
+    profile_name="sales",
+    target_name="iris",
+    profiles_yml_filepath="/usr/local/airflow/dags/sales/profiles.yml",
+)
+
+my_cosmos_dag = DbtDag(
+    project_config=ProjectConfig(
+        "/usr/local/airflow/dags/sales",
+    ),
+    profile_config=profile_config,
+    #execution_config=ExecutionConfig(
+    #    dbt_executable_path=f"/home/resingh/Documents/DBT/newdemo3/newvenv-env/bin/dbt",
+    #),
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="dbt_sales_dag",
+    default_args={"retries": 2},
+)
